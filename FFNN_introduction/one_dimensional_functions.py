6d2e8f99f501a634464caedf55b78108b6f1b987
klein-tu-da <62878150+klein-tu-da@users.noreply.github.com>
2024-04-15 08:37:35 +0200

    Changes after first Colab test

--- FFNN_introduction/one_dimensional_functions.py
+++ FFNN_introduction/one_dimensional_functions.py
@@ -20,9 +20,9 @@ import datetime
 now = datetime.datetime.now
 
 # %% Own modules
-import data as ld
-import models as lm
-import plots as lp
+import PAML_test.FFNN_introduction.data as ld
+import PAML_test.FFNN_introduction.models as lm
+import PAML_test.FFNN_introduction.plots as lp
 
 
 
@@ -48,7 +48,7 @@ def one_dimensional_functions(units, activation, non_neg, data, epochs):
     print(t1)
 
     tf.keras.backend.set_value(model.optimizer.learning_rate, 0.002)
-    h = model.fit([xs_c], [ys_c], epochs = 500,  verbose = 2)
+    h = model.fit([xs_c], [ys_c], epochs = epochs,  verbose = 2)
 
     t2 = now()
     print('it took', t2 - t1, '(sec) to calibrate the model')
